3d352d4dd13234fc4838d13b2985f8785151e87f
ide user ide_paraboom <paraboom@gmail.com>
2012-06-01 11:52:57 +0000

    Шаблоны, стили для вывода xml, сохранение xml обратно в файл

--- server.js
+++ server.js
@@ -3,12 +3,15 @@
 if(!process.env.NODE_ENV) process.env.NODE_ENV = 'development';
 
 var express = require('express');
+var jade = require('jade');
 var app = express.createServer();
 var fs = require('fs');
 var util = require('util');
 var DomJS = require("dom-js").DomJS;
 
 app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
     app.use(express.logger());
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
@@ -26,7 +29,7 @@ app.configure('production', function () {
     app.use(express.errorHandler());
 });
 
-function readLines(file, callback) {
+function readFile(file, callback) {
     var result = '';
     
     file.on('data', function(data){
@@ -40,41 +43,29 @@ function readLines(file, callback) {
     });
 };
 
-function nodeInfo(node) {
-    console.log(" ");
-    console.log(" ");
-    console.log("==========================");
-    console.log("==========================");
-    console.log(node.nodeName, ' - ', node.nodeType, ' - ', node.nodeValue, ' parent - ', node.parentNode.nodeName);
-    console.log("==========================");
-    console.log("==========================");
-    console.log("ChildNodes length - ", node.childNodes.length);
-    for (var i=0, l=node.childNodes.length; i<l; i++) {
-        console.log('Child #', i, ' is ', node.childNodes[i].nodeName);
-    }
-    console.log(" ");
-    console.log(" ");
-}
-
-function createXML(str) {
+function createXML(str, callback) {
     var parser = new DomJS();
     
     parser.parse(str, function(err, dom){
         dom.children[0].name = 'newnode';
-        console.log("serializes to : " + dom.toXml());
-    });
-        
+        callback(dom);
+        // console.log("serializes to : " + dom.toXml());
+    });    
 }
 
-readLines(fs.createReadStream('data/example.xml'), function(result){
-    createXML(result);
-})
-
-app.get('/', function (req, res) {
-    readLines(fs.createReadStream('data/example.xml'), function(result){
-        createXML(result, res);
-        res.send(result);
-    })
+app.get('/', function(req, res){
+    readFile(fs.createReadStream('data/example.xml'), function(result){
+        result = result.replace(/[\n\t]/g, '').replace(/>\s*</g, '><');
+        createXML(result, function(doc){
+            res.render('index', {
+                title: 'XML Document Editing',
+                doc: doc
+            });
+            fs.writeFile('data/example.xml', doc.toXml(), function(err){
+                if (err) console.log(err);
+            });
+        });
+    });
 });
 
 
