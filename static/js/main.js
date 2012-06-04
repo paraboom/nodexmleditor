91a56bb1d01a2aac40fb7cbfa5a93381669e1456
ide user ide_paraboom <paraboom@gmail.com>
2012-06-04 10:56:43 +0000

    Специальные иконки для редактирования, выбор контента редактируемой ноды по клику

--- static/js/main.js
+++ static/js/main.js
@@ -12,9 +12,28 @@ xmlOperations = {
 }
 
 $(function(){
-    var textNodes = $('.xml-node-text-value');
+    var container = $('#xml_container'),
+        editableNodes = container.find('.xml-editable'),
+        iconsPanel = editableNodes.append('<span class="xml-edit-panel" contentEditable="false"></span>').find('.xml-edit-panel');
+        
+    iconsPanel.append('<i class="icon-edit" /><i class="icon-ok-sign" /><i class="icon-remove-sign" />');
     
-    textNodes.on('click', function(){
+    container.on('click', '.icon-edit', function(evt){
+        var node = $(this).closest('.xml-editable');
+        
+        node.toggleClass('active', true).attr('contentEditable', 'true').focus();
+        
+        var range = document.createRange();
+        console.log(node[0].firstChild);
+        range.setStart(node[0].firstChild, 0);
+        range.setEnd(node[0].firstChild, node[0].firstChild.length);
+        
+        var selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+    });
+    
+    /*textNodes.on('click', function(){
         var el = $(this);
         
         el.attr('contentEditable', 'true');
@@ -30,5 +49,5 @@ $(function(){
         el.data('timeout', setTimeout(function(){
             xmlOperations.saveData(el);
         }, 1000));
-    });
+    });*/
 });
